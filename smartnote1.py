65c629c4ef15b7fe5022e938746ea9c8da72dd25
rania12-12 <pekaco92@gmail.com>
2026-08-04 14:46:43 +0300

    Update rania

--- smartnote1.py
+++ smartnote1.py
@@ -1,6 +1,7 @@
 from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import QApplication,QWidget,QLabel,QPushButton,QListWidget,QTextEdit,QVBoxLayout,QHBoxLayout,QLineEdit,QInputDialog
 import json
+#rania amaireh
 notes={
 "About planets" : 
      	{
